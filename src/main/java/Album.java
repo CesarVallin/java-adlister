f3fd1aadf19aecd04d81bfa16dd0542d76780d3c
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-29 16:52:12 -0500

    working on javabeans

---
+++ src/main/java/Album.java
@@ -0,0 +1,13 @@
+public class Album {
+    private int id;
+    private String artist;
+    private String name;
+    private int release_date;
+    private double sales;
+    private String genre;
+
+    /** Constructor */
+    public Album() {
+    }
+
+}
