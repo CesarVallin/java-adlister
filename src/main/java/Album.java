0177748f4dc2ff6546af5fd7d4658c9375e6090e
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-29 23:41:11 -0500

    created Album class with constructor, getters & setters

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -6,8 +6,65 @@ public class Album {
     private double sales;
     private String genre;
 
-    /** Constructor */
+    /** Constructors */
     public Album() {
     }
 
+    public Album(String artist, String name, int release_date, double sales, String genre) {
+        this.artist = artist;
+        this.name = name;
+        this.release_date = release_date;
+        this.sales = sales;
+        this.genre = genre;
+    }
+
+    /** Getters and Setters */
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(int release_date) {
+        this.release_date = release_date;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
